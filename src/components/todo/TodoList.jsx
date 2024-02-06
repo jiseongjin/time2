056b823407418271c72b268145ba83fc41ac9739
Your Name <you@your-domain.com>
2024-02-06 16:00:35 +0900

    3번까지

---
+++ src/components/todo/TodoList.jsx
@@ -0,0 +1,29 @@
+import TodoItem from "./TodoItem";
+
+const TodoList = ({ headTitle, todos, onDeleteTodoItem, onToggleTodoItem }) => {
+  return (
+    <section>
+      <h2>{headTitle}</h2>
+      <ul>
+        {
+          // SECTION: 1번 문제
+          // TODO: 투두 리스트 렌더링
+          // NOTE: map 메서드를 사용하여 `TodoItem` 컴포넌트 렌더링
+          // HINT: `TodoItem` 컴포넌트에는 `todo`, `onDeleteTodoItem`, `onToggleTodoItem` props를 전달
+        }
+        {todos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onDeleteTodoItem={onDeleteTodoItem}
+              onToggleTodoItem={onToggleTodoItem}
+            />
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default TodoList;
